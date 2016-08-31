1f5f5b426bc8c777d83d8179984f6cfdbafb0c83
fcggamou <fcggamou@gmail.com>
2016-08-31 19:40:33 -0300

    Agregada función para calcular information gain deun atributo

--- practico-2/ejercicio_6.py
+++ practico-2/ejercicio_6.py
@@ -3,21 +3,23 @@ import math
 import reader
 
 def main():
-	# attrs=['a','b','target']
-	# data=np.array([[1,2,1], [1,4,1], [1,6,1], [1,2,1], [1,4,1], [1,6,1], [1,3,0], [1,3,0]])
-	fileName = "data/student-mat.csv"
-	csvData = reader.getDataFromCsv(fileName)
-	attrs, data = csvData[0], np.array(csvData[1])
+    #attrs=['a','b','target']
+    #data=np.array([[1,2,1], [1,4,1], [1,6,1], [1,2,1], [1,4,1], [1,6,1], [1,3,0], [2,3,0]])
+    fileName = "data/student-mat.csv"
+    csvData = reader.getDataFromCsv(fileName)
+    attrs, data = csvData[0], np.array(csvData[1])
 	#Imprime la entropia del conjunto original con respecto a G3
-	print entropy(attrs, data, 'G3')
-
+    print entropy(attrs, data, 'G3')
+	#Imprime el information gain del atributo 'Walc' respecto a G3.
+    print informationGain(attrs, data, 'Walc', 'G3')    
+	
 #Retorna la entropia de un conjunto de datos para un determinado atributo objetivo.
 def entropy(attributes, data, targetAttr):
 
     i = attributes.index(targetAttr)
     targetValues = data[:,i]
 	
-	#Frecuencia de cada valor del target atributo objetivo dentro del conjunto de datos.
+	#Frecuencia de cada valor del atributo objetivo dentro del conjunto de datos.
     valFreq = {}
     entropy = 0.0
 
@@ -32,5 +34,27 @@ def entropy(attributes, data, targetAttr):
         entropy += (-proportion) * math.log(proportion, 2) 
         
     return entropy
+
+# Retorna el information gain para el atributo attr y el atributo objetivo targetAttr.
+def informationGain(attributes, data, attr, targetAttr):    
+
+    i = attributes.index(attr)
+    attrValues = data[:,i]
+	
+    valFreq = {}
+    for attrValue in attrValues:
+        if (valFreq.has_key(attrValue)):
+            valFreq[attrValue] += 1.0
+        else:
+            valFreq[attrValue]  = 1.0
+	
+    attrEntropy = 0.0
+    dataLength = len(data)
+    for val in valFreq.keys():
+        valProb        = valFreq[val] / dataLength
+        attrData     = np.array([entry for entry in data if entry[i] == val])
+        attrEntropy += valProb * entropy(attributes, attrData, targetAttr)
+ 
+    return (entropy(attributes, data, targetAttr) - attrEntropy)
 	
 main()
