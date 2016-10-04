b51445c871d5aab6ba898a88fb7e996738fb7cfe
Marccio Silva <marccio.silva@fing.edu.uy>
2016-10-04 03:32:44 -0300

    Ejercicio 3: Modificadas pruebas para cada funcion

--- practico-4/ej-3.py
+++ practico-4/ej-3.py
@@ -71,6 +71,7 @@ class Network:
 	def backPropagation(self, targets, N):	
 
 		# Delta para los nodos de salida
+		# Genero lista para los errores de la salida de la capa de output.
 		outputError = [0.0] * self.output
 		for k in range(self.output):
 			outputError[k] = dsigmoid(self.xo[k]) * (targets[k]-self.xo[k])
@@ -88,12 +89,12 @@ class Network:
 		for k in range(len(targets)):
 			error = error + 0.5*(targets[k]-self.xo[k])**2
 				
-		# Recalculo pesos de salida usando el factor de aprendizaje N
+		# Recalculo pesos de salida usando el factor de aprendizaje N.
 		for j in range(self.hidden):
 			for k in range(self.output):
 				self.wo[j][k] = self.wo[j][k] + N * outputError[k] * self.xh[j]				
 
-		# Recalculp pesos de nodos de entrada
+		# Recalculo pesos de nodos de capa oculta usando el factor de aprendizaje N.
 		for i in range(self.input):
 			for j in range(self.hidden):
 				self.wi[i][j] = self.wi[i][j] + N* hiddenError[j] * self.xi[i]
@@ -102,42 +103,108 @@ class Network:
 
 	def test(self, patterns):
 		for p in patterns:
-			print( p[0][0], self.update(p[0])[0])
+			print( p[0], self.update(p[0])[0])
 			#print(p[0], '->', self.update(p[0]))
 	
 	# Entrena a la red neuronal	
 	# N=factor de aprendizaje
-	def train(self, examples, N=0.5, maxIters=1000):
+	def train(self, examples, N, maxIters):
 		i=0
 		while (i<maxIters):
 			error = 0.0
 			for p in examples:
 				inputs = p[0]
 				targets = p[1]
+				# Evalua inputs en la red actual (forward propagation)
 				self.update(inputs)				
 				error = error + self.backPropagation(targets, N)				
 			i=i+1
 		print('error %-.5f' % error)
 
 def sen():
-	return [[[x/20.0], [round(math.sin(x*math.pi*1.5),3)]] for x in range(-20,21)]
+	# return [[[x/20.0], [round(math.sin(x*math.pi*1.5),3)]] for x in range(-20,19)]
+	# 40 valores equiespaciados entre -1.0 y 1.0
+	lst = []
+	domain = numpy.arange(-1, 1, 0.05)
+	for number in domain:
+		lst.append([[number],[round(math.sin(number*math.pi*1.5),3)]])
+	return lst
 	
-def senTest():
-	return [[[x/100.0], [round(math.sin(x*math.pi*1.5),3)]] for x in range(-100,101)]
+# def senTest():
+# 	return [[[x/100.0], [round(math.sin(x*math.pi*1.5),3)]] for x in range(-10,10)]
 	
-def qtest():
-	return [[[x/100.0], [round((x/100.0)**2,3)]] for x in range(-100, 101)]
+# def qtest():
+# 	return [[[x/100.0], [round((x/100.0)**2,3)]] for x in range(-100, 101)]
 
 def quadr():
-	return [[[x/20.0], [round((x/20.0)**2,3)]] for x in range(-20,21)]
-	
+	lst = []
+	#40
+	domain = numpy.arange(-1, 1, 0.05)
+	for number in domain:
+		lst.append([[number],[number**2]])
+	return lst
+
+def h(begin, end, step):
+	# Numeros equiespaciados entre begin y end, separados por step
+	x = numpy.arange(begin, end, step)
+	X, Y = numpy.meshgrid(x, x)
+	# Se obtienen len(x)**2 numeros equiespaciados entre (-1.0,-1.0) y (1.0,1.0)
+	puntosEquiespaciados = [numpy.array(thing) for thing in zip(X.flatten(), Y.flatten())]
+	# print puntosEquiespaciados
+	setSize = len(puntosEquiespaciados)
+	# Genero el set de entrenamiento
+	trainingValues = []
+	for i in range(setSize):
+	# for punto in puntosEquiespaciados:
+		x = puntosEquiespaciados[i][0]
+		y = puntosEquiespaciados[i][1]
+		imagen = 0.0
+		# Aplico funcion h(x)
+		if (x**2 + y**2 < 0.5):
+			imagen = 1.0
+		trainingValues.append([[x, y],[imagen]])
+	return trainingValues
+
 def main():
 	random.seed(0)
-	print sen()
-	#x^2
-	n = Network(1, 100, 1)
-	n.train(sen())
-	n.test(senTest())
+	setIteraciones = [10**2, 10**3, 10**4, 10**5]
+	N = 0.5
+	print "#############Test funcion seno#############"
+	print "#############Funcion original#############"
+	for element in sen():
+		print (element[0][0], element[1][0])
+	for iteraciones in setIteraciones:
+		print "#############Test con " + str(iteraciones) + " iteraciones#############"
+		n = Network(1, 20, 1)
+		n.train(sen(), N, iteraciones)
+		# Se testea contra los datos de entrada a ver que tanto llega a asimilarse a la funcion
+		# original para esos puntos.		
+		n.test(sen())
+		# n.test(senTest())
+	print "#############Funcion original#############"
+	for element in quadr():
+		print (element[0][0], element[1][0])	
+	print "#############Test funcion cuadratica#############"
+	for iteraciones in setIteraciones:
+		print "#############Test con " + str(iteraciones) + " iteraciones#############"
+		n = Network(1,20,1)
+		n.train(quadr(), N, iteraciones)
+		# Se testea contra los datos de entrada a ver que tanto llega a asimilarse a la funcion
+		# original para esos puntos.
+		n.test(quadr())
+		# n.test(qtest())
+	print "#############Test funcion h(x)#############"
+	print "#############Funcion original#############"
+	print h(-1.0,1.1,0.4)	
+	for element in h(-1.0,1.1,0.4):
+		print ((element[0][0], element[0][1]), element[1][0])
+	for iteraciones in setIteraciones:
+		print "#############Test con " + str(iteraciones) + " iteraciones#############"
+		n = Network(1,20,1)
+		n.train(h(-1.0,1.1,0.4), N, iteraciones)
+		# Se testea contra los datos de entrada a ver que tanto llega a asimilarse a la funcion
+		# original para esos puntos.
+		n.test(h(-1.0,1.1,0.4))
 
 main()
 
